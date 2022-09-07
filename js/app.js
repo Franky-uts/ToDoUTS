f679da403f68cd2c3a346374ba00bf3a1aa91f0e
Franky-uts <107886319+Franky-uts@users.noreply.github.com>
2022-09-07 12:15:41 -0500

    Update

--- js/app.js
+++ js/app.js
@@ -8,6 +8,10 @@ let Tareas = {}
 
 document.addEventListener('DOMContentLoaded', () => {
     console.log('Cargo pagina')
+    if(localStorage.getItem('tareas')){
+        Tareas = JSON.parse(localStorage.getItem('tareas'))
+        pintarTareas()
+    }
 })
 
 formulario.addEventListener('submit',event =>{
@@ -15,3 +19,40 @@ formulario.addEventListener('submit',event =>{
     //console.log('evento',event)
     setTarea(event)
 })
+const setTarea = e => {
+    const texto = e.target.querySelector('input').value
+    //console.log(texto)
+    if(texto.trim()===' '){
+        console.log('Cadena vacia')
+        return
+    }
+    const tarea = {
+        id: Date.now(),
+        texto,
+        estado: false
+    }
+    //console.log('Tarea',tarea)
+    Tareas[Tareas.id]=tarea
+    pintarTareas()
+    formulario.request()
+    e.target.querySelector('imput').focus()
+}
+const pintarTareas = () =>{
+    localStorage.setItem('tareas',JSON.stringify(Tareas))
+    if (Object.values(Tareas).length === 0) {
+        listatareas.innerHTML = ''
+        Object.values(Tareas).forEach(item=>{
+            const clone = template.cloneNode(True)
+            clone.querySelector('p').textContent = item.texto
+            if(item.estado){
+                clone.querySelectorAll('.fas')[0].classList.replace('fa-check-circle','fa-undo-alt')
+                clone.querySelector('.alert').classList.replace('alert-warning', 'alert-primary')
+                clone.querySelector('p').style.textDecoration = 'line-through'
+            }
+            clone.querySelectorAll('.fas')[0].dataset.id = item.id
+            clone.querySelectorAll('.fas')[1].dataset.id = item.id
+            fragment.appendChild(clone)
+        })
+        listatareas.appendChild(fragment)
+    }
+}
