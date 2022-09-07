8ab401481d757132736312628ab02cae1ff1e1d0
Franky-uts <107886319+Franky-uts@users.noreply.github.com>
2022-09-07 11:21:43 -0500

    SI1

--- js/app.js
+++ js/app.js
@@ -3,3 +3,15 @@ const formulario = document.getElementById('Formulario')
 const listatareas = document.getElementById('listadetareas')
 const template = document.getElementById('template').content
 const fragment = document.createDocumentFragment()
+
+let Tareas = {}
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Cargo pagina')
+})
+
+formulario.addEventListener('submit',event =>{
+    event.preventDefault()
+    //console.log('evento',event)
+    setTarea(event)
+})
