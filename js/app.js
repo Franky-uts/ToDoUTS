cd245a4cf65e5d801c62402415995b8a55528854
Franky-uts <107886319+Franky-uts@users.noreply.github.com>
2022-09-07 11:10:54 -0500

    Por ahora (07/09)

--- js/app.js
+++ js/app.js
@@ -1,1 +1,5 @@
 console.log('Hola mundo :)')
+const formulario = document.getElementById('Formulario')
+const listatareas = document.getElementById('listadetareas')
+const template = document.getElementById('template').content
+const fragment = document.createDocumentFragment()
